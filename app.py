aa489fb23fe7048d19fd3a1f8ad6744d5e318102
Sonal Dubey <sonaldubey@unknown20c9d089ec5b.gateway.2wire.net>
2013-05-04 15:47:20 -0700

    added the initial flow in index.html

--- app.py
+++ app.py
@@ -6,8 +6,18 @@ from bottle import route, run, template
 #    return template('<b>Hello {{name}}</b>!', name=name)
 
 
+
+def read(filename):
+	with open(filename, 'r') as f:
+		return f.read()
+
 @route('/')
 def index():
-    return 'Hello Sonal'
+    return read('home.html')
+
+@route('/trend')
+def trend():
+    return 'I am so stupid'
+
 
 run(host='localhost', port=8080)
